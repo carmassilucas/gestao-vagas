8f49c246a31555b71d7f037543c6d5d897379303
Lucas Carmassi <carmassilucas@gmail.com>
2023-11-20 01:30:47 -0300

    implementing findJobByFilter route and customizing documentation

--- src/main/java/br/com/ignite/rocketseat/gestaovagas/GestaoDeVagasApplication.java
+++ src/main/java/br/com/ignite/rocketseat/gestaovagas/GestaoDeVagasApplication.java
@@ -1,17 +1,25 @@
 package br.com.ignite.rocketseat.gestaovagas;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Gestão de vagas",
-				description = "Api responsável pela gestão de vagas",
-				version = "1"
-		)
+@OpenAPIDefinition(info = @Info(
+		title = "Gestão de vagas",
+		description = "Api responsável pela gestão de vagas",
+		version = "1"
+))
+@SecurityScheme(
+		name = "jwt_auth",
+		scheme = "bearer",
+		bearerFormat = "JWT",
+		type = SecuritySchemeType.HTTP,
+		in = SecuritySchemeIn.HEADER
 )
 public class GestaoDeVagasApplication {
 
--- src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/candidates/controllers/CandidateController.java
+++ src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/candidates/controllers/CandidateController.java
@@ -2,7 +2,17 @@ package br.com.ignite.rocketseat.gestaovagas.modules.candidates.controllers;
 
 import br.com.ignite.rocketseat.gestaovagas.modules.candidates.CandidateEntity;
 import br.com.ignite.rocketseat.gestaovagas.modules.candidates.useCases.CreateCandidateUseCase;
+import br.com.ignite.rocketseat.gestaovagas.modules.candidates.useCases.ListAllJobByFilterUseCase;
 import br.com.ignite.rocketseat.gestaovagas.modules.candidates.useCases.ProfileCandidateUseCase;
+import br.com.ignite.rocketseat.gestaovagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -19,6 +30,8 @@ public class CandidateController {
     private CreateCandidateUseCase createCandidateUseCase;
     @Autowired
     private ProfileCandidateUseCase profileCandidateUseCase;
+    @Autowired
+    private ListAllJobByFilterUseCase listAllJobByFilterUseCase;
 
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -43,5 +56,24 @@ public class CandidateController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(
+            name = "Candidate",
+            description = "candidate informations"
+    )
+    @Operation(
+            summary = "Listagem de vagas disponíveis para o candidato",
+            description = "Essa função é responsável por listar todas as vagas disponíveis baseadas no filtro"
+    )
+    @ApiResponses({@ApiResponse(
+            responseCode = "200",
+            content = {@Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))})
+    })
+    @SecurityRequirement(name = "jwt_auth")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+        return this.listAllJobByFilterUseCase.execute(filter);
     }
 }
