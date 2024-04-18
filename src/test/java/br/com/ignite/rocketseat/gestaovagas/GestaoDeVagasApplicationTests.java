2cf3abd394a9ca83515461d10ac589deaa484a7f
Lucas Carmassi <carmassilucas@gmail.com>
2024-04-18 16:07:42 -0300

    projeto base gerado pelo spring initializr

---
+++ src/test/java/br/com/ignite/rocketseat/gestaovagas/GestaoDeVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ignite.rocketseat.gestaovagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
