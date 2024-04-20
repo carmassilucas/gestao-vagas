ac7d82a333fe63785d1c0e945ccaaa640ea4f406
Lucas Carmassi <carmassilucas@gmail.com>
2024-04-20 15:59:36 -0300

    codificando senha no cadastro da entidade candidate

--- src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import br.com.ignite.rocketseat.gestaovagas.exceptions.UserFoundException;
 import br.com.ignite.rocketseat.gestaovagas.modules.candidate.CandidateEntity;
 import br.com.ignite.rocketseat.gestaovagas.modules.candidate.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,14 +12,21 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
-    public CandidateEntity execute(CandidateEntity candidate) {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository
-                .findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail())
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
 
-        return this.candidateRepository.save(candidate);
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
+
+        return this.candidateRepository.save(candidateEntity);
     }
 
 }
