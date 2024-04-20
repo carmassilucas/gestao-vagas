a224b092c44a5e26de804b68c08f01337eefb1b0
Lucas Carmassi <carmassilucas@gmail.com>
2024-04-19 22:33:20 -0300

    alterando CandidateController para interagir com useCase

--- src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,12 @@
 package br.com.ignite.rocketseat.gestaovagas.modules.candidate.controllers;
 
+import br.com.ignite.rocketseat.gestaovagas.exceptions.UserFoundException;
 import br.com.ignite.rocketseat.gestaovagas.modules.candidate.CandidateEntity;
+import br.com.ignite.rocketseat.gestaovagas.modules.candidate.CandidateRepository;
+import br.com.ignite.rocketseat.gestaovagas.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+    @Autowired
+    CreateCandidateUseCase createCandidateUseCase;
 
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidate) {
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try {
+            var result = this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
 
     }
 }
