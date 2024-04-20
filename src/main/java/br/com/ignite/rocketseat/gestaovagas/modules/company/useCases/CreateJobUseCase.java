99f7bfdf9f885a64078e1b826b21b3b4a482ceef
Lucas Carmassi <carmassilucas@gmail.com>
2024-04-20 11:37:58 -0300

    criando fluxo de cadastro da entidade job

---
+++ src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/company/useCases/CreateJobUseCase.java
@@ -0,0 +1,16 @@
+package br.com.ignite.rocketseat.gestaovagas.modules.company.useCases;
+
+import br.com.ignite.rocketseat.gestaovagas.modules.company.entities.JobEntity;
+import br.com.ignite.rocketseat.gestaovagas.modules.company.repositories.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateJobUseCase {
+    @Autowired
+    private JobRepository jobRepository;
+
+    public JobEntity execute(JobEntity jobEntity) {
+        return this.jobRepository.save(jobEntity);
+    }
+}
