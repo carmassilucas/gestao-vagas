9e84c3039f3085a2ba36202ec52416147a1b88f7
Lucas Carmassi <carmassilucas@gmail.com>
2024-04-25 17:24:23 -0300

    adicionando documentação nos controllers

--- src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/candidate/controllers/CandidateController.java
@@ -1,8 +1,19 @@
 package br.com.ignite.rocketseat.gestaovagas.modules.candidate.controllers;
 
 import br.com.ignite.rocketseat.gestaovagas.modules.candidate.CandidateEntity;
+import br.com.ignite.rocketseat.gestaovagas.modules.candidate.dtos.ProfileCandidateResponseDto;
 import br.com.ignite.rocketseat.gestaovagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.ignite.rocketseat.gestaovagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.ignite.rocketseat.gestaovagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import br.com.ignite.rocketseat.gestaovagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +21,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Candidato", description = "Informações do candidato")
 public class CandidateController {
     @Autowired
     CreateCandidateUseCase createCandidateUseCase;
@@ -21,7 +34,18 @@ public class CandidateController {
     @Autowired
     private ProfileCandidateUseCase profileCandidateUseCase;
 
+    @Autowired
+    private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
+
     @PostMapping
+    @Operation(summary = "Cadastro de candidato",
+            description = "Essa função é responsável por cadastrar um candidato")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+                @Content(schema = @Schema(implementation = CandidateEntity.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Usuário já existe")
+    })
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
         try {
             var result = this.createCandidateUseCase.execute(candidateEntity);
@@ -33,6 +57,15 @@ public class CandidateController {
 
     @GetMapping
     @PreAuthorize("hasRole('CANDIDATE')")
+    @Operation(summary = "Perfil do candidato",
+            description = "Essa função é responsável por buscar as informações do perfil do candidato")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = ProfileCandidateResponseDto.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Usuário não encontrado")
+    })
+    @SecurityRequirement(name = "jwt_auth")
     public ResponseEntity<Object> get(HttpServletRequest request) {
         var idCandidate = request.getAttribute("candidate_id");
 
@@ -42,5 +75,19 @@ public class CandidateController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @Operation(summary = "Listagem de vagas disponíveis para o candidato",
+            description = "Essa função é responsável por listar todas as vagas disponíveis baseada no filtro")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))
+            })
+    })
+    @SecurityRequirement(name = "jwt_auth")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+        return this.listAllJobsByFilterUseCase.execute(filter);
     }
 }
--- src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/company/controllers/JobController.java
@@ -3,6 +3,13 @@ package br.com.ignite.rocketseat.gestaovagas.modules.company.controllers;
 import br.com.ignite.rocketseat.gestaovagas.modules.company.dtos.CreateJobDto;
 import br.com.ignite.rocketseat.gestaovagas.modules.company.entities.JobEntity;
 import br.com.ignite.rocketseat.gestaovagas.modules.company.useCases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +30,15 @@ public class JobController {
 
     @PostMapping
     @PreAuthorize("hasRole('COMPANY')")
+    @Tag(name = "Vagas", description = "Informações das vagas")
+    @Operation(summary = "Cadastro de vaga",
+            description = "Essa função é responsável por cadastrar as vagas dentro da empresa")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = JobEntity.class))
+            })
+    })
+    @SecurityRequirement(name = "jwt_auth")
     public JobEntity create(@Valid @RequestBody CreateJobDto createJobDto, HttpServletRequest request) {
         var companyId = request.getAttribute("company_id");
 
