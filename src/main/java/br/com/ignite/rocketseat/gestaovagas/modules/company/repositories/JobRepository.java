df09c277a17471138c8fbccb954f15e31652a008
Lucas Carmassi <carmassilucas@gmail.com>
2024-04-25 17:23:22 -0300

    criando busca no repositório da entidade job

--- src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/company/repositories/JobRepository.java
+++ src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/company/repositories/JobRepository.java
@@ -3,8 +3,9 @@ package br.com.ignite.rocketseat.gestaovagas.modules.company.repositories;
 import br.com.ignite.rocketseat.gestaovagas.modules.company.entities.JobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
-
+        List<JobEntity> findByDescriptionContainingIgnoreCase(String filter);
 }
