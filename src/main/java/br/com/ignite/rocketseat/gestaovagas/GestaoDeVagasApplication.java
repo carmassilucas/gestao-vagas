d63a25e40aa94b48b15729cf06860f6a33ad8228
Lucas Carmassi <carmassilucas@gmail.com>
2024-04-25 17:21:59 -0300

    removendo configurações do swagger da classe application

--- src/main/java/br/com/ignite/rocketseat/gestaovagas/GestaoDeVagasApplication.java
+++ src/main/java/br/com/ignite/rocketseat/gestaovagas/GestaoDeVagasApplication.java
@@ -1,17 +1,8 @@
 package br.com.ignite.rocketseat.gestaovagas;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Gestão de vagas",
-				description = "APIRestful responsável pela gestão de vagas",
-				version = "1.0.0"
-		)
-)
 @SpringBootApplication
 public class GestaoDeVagasApplication {
 
---
+++ src/main/java/br/com/ignite/rocketseat/gestaovagas/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package br.com.ignite.rocketseat.gestaovagas.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Gestão de vagas").description("APIRestful responsável pela gestão de vagas").version("1"))
+                .schemaRequirement("jwt_auth", createSecurityScheme());
+    }
+
+    private io.swagger.v3.oas.models.security.SecurityScheme createSecurityScheme() {
+        return new SecurityScheme().name("jwt_auth").type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT");
+    }
+}
