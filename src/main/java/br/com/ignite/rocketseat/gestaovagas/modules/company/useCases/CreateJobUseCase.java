6c8241ad8395b8c372e4e61efbfaf5ecbaf649dd
Lucas Carmassi <carmassilucas@gmail.com>
2024-05-03 20:52:08 -0300

    verificando se company existe ao cadastrar um job

--- src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/company/useCases/CreateJobUseCase.java
+++ src/main/java/br/com/ignite/rocketseat/gestaovagas/modules/company/useCases/CreateJobUseCase.java
@@ -1,6 +1,8 @@
 package br.com.ignite.rocketseat.gestaovagas.modules.company.useCases;
 
+import br.com.ignite.rocketseat.gestaovagas.exceptions.CompanyNotFoundException;
 import br.com.ignite.rocketseat.gestaovagas.modules.company.entities.JobEntity;
+import br.com.ignite.rocketseat.gestaovagas.modules.company.repositories.CompanyRepository;
 import br.com.ignite.rocketseat.gestaovagas.modules.company.repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +12,13 @@ public class CreateJobUseCase {
     @Autowired
     private JobRepository jobRepository;
 
+    @Autowired
+    private CompanyRepository companyRepository;
+
     public JobEntity execute(JobEntity jobEntity) {
+        this.companyRepository.findById(jobEntity.getCompanyId())
+                .orElseThrow(CompanyNotFoundException::new);
+
         return this.jobRepository.save(jobEntity);
     }
 }
